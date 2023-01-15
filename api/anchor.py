1aed3b2ae62cdca71673863472e5d1d2083ea5d7
jackluson <luxuemin2108@gmail.com>
2023-01-15 23:48:03 +0800

    feat: add stock services

---
+++ api/anchor.py
@@ -0,0 +1,31 @@
+'''
+Desc:
+File: /anchor.py
+File Created: Sunday, 15th January 2023 9:03:35 pm
+Author: luxuemin2108@gmail.com
+-----
+Copyright (c) 2022 Camel Lu
+'''
+import sys
+import os
+sys.path.append(os.getcwd() + '/')
+from base.base_api_config import BaseApiConfig
+import requests
+
+class AnchorApi(BaseApiConfig):
+    def __init__(self):
+        super().__init__()
+        self.base_url = 'http://127.0.0.1:5000/api'
+    def get_stock_profile(self, stock_code):
+        payload = {
+            'code': stock_code,
+        }
+        url = self.base_url + "/stock_profile?code={code}".format(
+            **payload)
+        res = requests.get(url)
+        print("res", res)
+        print(res.json())
+if __name__ == '__main__':
+    anchorApi = AnchorApi()
+    code = '000005'
+    anchorApi.get_stock_profile(code)
--- services/stock/app.py
+++ services/stock/app.py
@@ -8,7 +8,8 @@ Copyright (c) 2022 Camel Lu
 '''
 import sys
 sys.path.append('./')
-from flask import Flask, Blueprint
+import json
+from flask import Flask, Blueprint, Response, jsonify
 from flask_apispec import marshal_with, use_kwargs
 from marshmallow import fields
 from datetime import datetime
@@ -18,25 +19,31 @@ from sqlalchemy.orm import validates, Session
 from models.stock import StockProfile 
 from models.var import engine
 
-
+def toJSON(self):
+    return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 session = Session(engine)
-print("__name__", __name__)
+# print("__name__", __name__)
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 blueprint = Blueprint('/api', __name__)
 
 @app.route("/api/stock_profile", methods=('GET',))
-@use_kwargs({'code': fields.Str()})
-def get_stock_profile(code=None):
-    print("code", code)
-    print('jisd')
+@use_kwargs({'code': fields.Str()}, location="query")
+def get_stock_profile(code, **kwargs):
+    print("code", code, kwargs)
     res = {
         'text': "Hello, World!"
     }
-    stock_code = '000004'
-    stock = session.query(StockProfile).where(StockProfile.stock_code == stock_code).all()
-    print("stock", stock)
-    return stock[0].stock_name
-
+    # stock_code = '000004'
+    stock = session.query(StockProfile).where(StockProfile.stock_code == code).all()
+    print("stock", stock, stock[0].__dict__)
+    # return Response(stock[0].stock_name, mimetype='text/plain')
+    res = {
+        'stock_name': stock[0].__dict__.get('stock_name'),
+        'classi_name': stock[0].__dict__.get('classi_name'),
+    }
+    print(type(jsonify(res)))
+    return jsonify(res)
 
 # export FLASK_APP=hello.py
 # export FLASK_ENV=development
